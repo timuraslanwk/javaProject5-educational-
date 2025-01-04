8c3b7ac6765e8d6e01784ebb1c0f4eb512d540cb
timuraslanwk <timuraslan.wk@gmail.com>
2025-01-04 09:54:05 +0300

    Task1 and Task2 completed.

--- src/task01/_01_Task.java
+++ src/task01/_01_Task.java
@@ -1,7 +1,71 @@
 package task01;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class _01_Task {
     public static void main(String[] args) {
-        System.out.println("test");
+
+        Scanner scnInt = new Scanner(System.in);
+        Scanner scnStr = new Scanner(System.in);
+
+        ArrayList<String> kitapAdlari = new ArrayList<>();
+        ArrayList<String> yazarAdlari = new ArrayList<>();
+
+        int secim;
+
+
+
+        do {
+
+            System.out.println("\n 1-Kitapları görüntüle.\n 2-Kitap ekle. \n 3-Kitap sil. \n 4-Çıkış.");
+            System.out.print("Seçiminizi yapınız(1-4): ");
+            secim = scnInt.nextInt();
+
+
+            switch (secim) {
+                case 1:
+                    if (kitapAdlari.isEmpty()) {
+                        System.out.println("\n Kütüphaneniz boş.");
+                    } else for (int i = 0; i < kitapAdlari.size(); i++) {
+                        System.out.println(kitapAdlari.get(i) + ": " + yazarAdlari.get(i));
+                    } break;
+                case 2:
+                    System.out.print("\nKitabın adı: ");
+                    String kitapAdi = scnStr.nextLine();
+                    System.out.print("Kitabın yazarı: ");
+                    String yazarAdi = scnStr.nextLine();
+
+                    kitapAdlari.add(kitapAdi);
+                    yazarAdlari.add(yazarAdi);
+                    break;
+
+                case 3:
+                    System.out.print("Silmek istediğiniz kitabın adını yazınız: ");
+                    String silinecekKitap = scnStr.nextLine();
+
+                    boolean bulunduMu = false;
+
+                    for (int i = 0; i < kitapAdlari.size(); i++) {
+                        if (kitapAdlari.get(i).equalsIgnoreCase(silinecekKitap)) {
+                            kitapAdlari.remove(i);
+                            bulunduMu = true;
+                            break;
+                        }
+                    }
+
+                    if (bulunduMu) {
+                        System.out.println(silinecekKitap + " kütüphanenizden kaldırılmıştır.\n");
+                    } else System.out.println("Girmiş olduğunuz kitap kütüphanede bulunmamaktadır.\n");
+                    break;
+
+                default:
+                    System.out.println("Geçerli bir seçim yapınız.");
+
+            }
+
+        }while (secim!=4);
+        System.out.println("\nProgram sonlandırılmıştır...");
+
     }
 }
--- src/task02/_02_Task.java
+++ src/task02/_02_Task.java
@@ -1,4 +1,89 @@
 package task02;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class _02_Task {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int secim;
+
+        do {
+
+            anaMenuyuGoster();
+            secim = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (secim) {
+                case 1:
+                    sifreEkle();
+                    break;
+                case 2:
+                    sifreGoster();
+                    break;
+                default:
+                    System.out.println("\n Lütfen geçerli bir tuşlama yapınız.\n");
+            }
+        } while (secim != 3);
+
+        System.out.println("\n Program sonlandırılmıştır...");
+
+    }
+
+    private static void anaMenuyuGoster() {
+
+        System.out.println("Ana Menü");
+        System.out.println("1. Şifre Ekle");
+        System.out.println("2. Şifre Göster");
+        System.out.println("3. Çıkış");
+        System.out.print("Seçiminizi yapın: ");
+
+    }
+
+    private static void sifreEkle() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nHesabınızın adını giriniz: ");
+        String hesapAdi = scanner.nextLine().toLowerCase();
+
+        System.out.print("Kullanıcı adını giriniz: ");
+        String kullaniciAdi = scanner.nextLine().toLowerCase();
+
+        System.out.print("Şifrenizi giriniz: ");
+        String sifre = scanner.nextLine();
+
+        Map<String, String> bilgiler = new HashMap<>();
+        bilgiler.put(kullaniciAdi, sifre);
+        veriDeposu.put(hesapAdi, bilgiler);
+
+        System.out.println("\n Şifreniz kaydedilmiştir. \n");
+
+    }
+
+    private static void sifreGoster() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nHesap adını giriniz: ");
+        String hesapAdi = scanner.nextLine().toLowerCase();
+
+        if (veriDeposu.containsKey(hesapAdi)) {
+            Map<String, String> kullaniciBilgileri = veriDeposu.get(hesapAdi);
+
+            for (Map.Entry<String, String> giris : kullaniciBilgileri.entrySet()) {
+                System.out.println("\nKullanıcı Adı: " + giris.getKey());
+                System.out.println("Şifre: " + giris.getValue()+ "\n");
+            }
+        } else {
+            System.out.println("\n Böyle bir kaydınız yoktur.\n");
+        }
+
+    }
+
+    private static Map<String, Map<String, String>> veriDeposu = new HashMap<>();
+
+
 }
